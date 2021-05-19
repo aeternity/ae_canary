85b8baebd51640835bf0ff405e64aa60d147f330
Dimitar Ivanov <dimitar.p.ivanov@gmail.com>
2021-05-19 16:50:06 +0300

    Improve the docker compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,15 @@ do
   sleep 1
 done
 
-mix ecto.setup
-mix run priv/repo/exchanges.exs
+export PGPASSWORD="${POSTGRES_PASSWORD}"
+if [[ `psql -Atqc "\\list ${POSTGRES_DB}" -h $POSTGRES_HOST -U $POSTGRES_USER postgres` ]]; then
+  echo "Database $POSTGRES_DB already exists."
+  mix ecto.migrate
+else
+  echo "Database $POSTGRES_DB does not exist. Creating..."
+  mix ecto.setup
+  mix run priv/repo/exchanges.exs
+  echo "Database $POSTGRES_DB created."
+fi
 
 exec mix phx.server
