831f1b070a19851beef66e5a8c588396d44d6970
Dimitar Ivanov <dimitar.p.ivanov@gmail.com>
2021-05-24 15:20:49 +0300

    Improve messages: allow longer messages and HTML support

---
+++ priv/repo/migrations/20210524120833_dashboard_msg_length.exs
@@ -0,0 +1,10 @@
+defmodule AeCanary.Repo.Migrations.DashboardMsgLength do
+  use Ecto.Migration
+
+  def change do
+    alter table(:dashboard) do
+      modify :message, :text
+    end
+
+  end
+end
