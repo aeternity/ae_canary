cb6a91e4aa48fcfbafd1b4aaa5125775966ed419
Fabian Krol <fabian@krol.io>
2021-11-10 16:29:12 +0100

    Add formatting check to test alias

--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule AeCanary.MixProject do
       setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["format --check-formatted", "ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
   end
 end
